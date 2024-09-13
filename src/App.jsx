edf285ac8b4b70c6658b787197ea5baac04b7a59
Lalmohan <pallalmohan48@gmail.com>
2024-09-13 23:39:59 +0530

    Complete Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
 import './styles.scss'
+import { useState } from 'react'
 import Board from './components/Board'
+import { CalculateWinner } from './components/CalculateWinner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+    const [xIsNext, setXIsNext] = useState(false);
+    
+    const winner = CalculateWinner(squares)
+    const nextPlayer = xIsNext? 'X' : '0'
+    const statusMassage = winner ? `Winner is ${winner}` : `Next player is ${nextPlayer}`
+
+    console.log("winner===",winner)
+    
+    const handleSquareClick = clickedPosition => {
+      if (squares[clickedPosition] || winner) {
+          return;
+      }
+      setSquares(currentSquares => {
+          return currentSquares.map((squareValue, position) => {
+              if (clickedPosition === position) {
+                  return xIsNext ? 'X' : '0';
+              }
+              return squareValue;
+          })
+      })
+
+      setXIsNext(currentXisNest => !currentXisNest)
+
+  }
 
   return (
     <>
     <div className="app">
-      <Board/>
+      <h2> {statusMassage}</h2>
+      <Board squares={squares} handleSquareClick={handleSquareClick}/>
     </div>
     </>
   )
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,44 +1,30 @@
-import { useState } from 'react'
 import '../styles.scss'
 import Square from "./Square"
 
-const Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
+const Board = ({squares,handleSquareClick}) => {
 
-    const handleSquareClick = clickedPosition =>{
-        setSquares(currentSquares => {
-            return currentSquares.map((squareValue, position) => {
-                if(clickedPosition === position ){
-                    return 'X';
-                }
-                return squareValue;
-            })
-        })
-        // 
-    }
-
-    const renderSquare = position =>{
+    const renderSquare = position => {
         return (
-            <Square value={squares[position]} onClick = {() => handleSquareClick(position)}/>
+            <Square value={squares[position]} onClick={() => handleSquareClick(position)} />
         )
     }
     return (
         <div className="board">
             <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-            
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+
             </div>
             <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     )
